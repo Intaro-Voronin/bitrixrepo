dc47d0849d34ceeb7172e814059a2979c2d10b68
Intaro-Voronin <voronin_s@intaro.email>
2021-05-25 14:38:42 +0300

    Search

---
+++ 10/www/bitrix/php_interface/include/functions-dump.php
@@ -0,0 +1,16 @@
+<?
+function test_dump($var, $die = false, $all = false)
+{
+	global $USER;
+	if($USER->IsAdmin() || ($all == true))
+	{
+		?>
+		<font style="text-align: left; font-size: 10px"><pre><?var_dump($var)?></pre></font><br>
+		<?
+	}
+	if($die)
+	{
+		die;
+	}
+}
+?>
